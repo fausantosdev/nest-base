3dbf0cd441e96b088a71c3f9f3a150c01ed9cda8
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-12-03 12:05:08 -0300

    feat: add Swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import '@config/sentry'
 
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
@@ -10,6 +11,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Base')
+    .setDescription('The NestJS Base API description')
+    .setVersion('1.0')
+    .addTag('nestjs, base, typescript, boilerplate, documentation')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory)
+
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
