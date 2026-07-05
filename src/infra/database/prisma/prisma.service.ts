52aa1fea302bae41334d05e7f2e22aeeae183742
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2026-07-04 22:42:43 -0300

    feat: implement Prisma lifecycle hooks for connection management

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 import { PrismaClient } from './generated/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     })
     super({ adapter })
+  }
+  async onModuleInit() {
+    return await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    return await this.$disconnect()
   }
 }
